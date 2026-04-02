547744495f6a5257395c537a85d1c9b259e6589e
Agatai01 <jangabil.nuralem@gmail.com>
2026-04-02 15:04:16 +0500

    Practice 8 completed

---
+++ Practice 8/connect.py
@@ -0,0 +1,14 @@
+# connect.py
+import psycopg2
+from config import DB_CONFIG
+
+def connect():
+    return psycopg2.connect(**DB_CONFIG)
+    
+def connect():
+    return psycopg2.connect(
+        host="localhost",
+        database="phonebook_db",
+        user="postgres",
+        password="12345678"
+    )
